a357238cc5ea2bc469ec213d834ee69f84e28aac
HakureiPOI <hakureipoi@qq.com>
2025-05-26 14:16:42 +0800

    update:虚拟机IP

--- include/config.h
+++ include/config.h
@@ -13,7 +13,7 @@
 #else
 #define NET_IF_IP        \
     {                    \
-        192, 168, 1, 1 \
+        192, 168, 56, 101 \
     }  // 自定义网卡ip地址
 #define NET_IF_MAC                         \
     {                                      \
